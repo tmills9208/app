f3c4c3088b57f45e3d0ea2ef803d198e46362b07
Tyler Mills <tmills9208@gmail.com>
2022-03-14 17:00:18 -0400

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -19,30 +19,6 @@ fn main() {
     let file_path = &args[2];
     let input = &args[3];
 
-    // Options are preferred, to show that they either contain a type, or nothing. no more nulls :p
-    // let hello: Option<&str> = Some("Hello, web browsers!");
-
-    // Use mutable when you know if the variable will be change beyon compile time
-    // let mut result: &str = match hello {
-    //     Some(ref x) => x,
-    //     None => ""
-    // };
-    
-    // Since its not being dynamically used, it is unneeded and you can simply reassign it
-    // by simply having let be declared for the same variable again.
-    // like below:
-    // let result: &str = match hello {
-    //     Some(ref x) => x,
-    //     None => ""
-    // };
-
-    // This will work too, may be even simpler just for single strings
-    // result = hello.map(|m| &m[..]).unwrap_or("");
-
-    // requires a string of type: &str
-    // println!("{}\n\n", result);
-
-    // let file_path = "./hello.txt";
     if (flag == "read" || flag == "-r") {
         file_controller::read_from_file(&file_path);
     }
@@ -59,3 +35,31 @@ fn main() {
         file_controller::write_to_file(&file_path, &new_line);
     }
 }
+
+
+// Im just going to keep this here, just some notes I was taking, but have no where else to put them atm.
+
+// Options are preferred, to show that they either contain a type, or nothing. no more nulls :p
+// let hello: Option<&str> = Some("Hello, web browsers!");
+
+// Use mutable when you know if the variable will be change beyon compile time
+// let mut result: &str = match hello {
+//     Some(ref x) => x,
+//     None => ""
+// };
+
+// Since its not being dynamically used, it is unneeded and you can simply reassign it
+// by simply having let be declared for the same variable again.
+// like below:
+// let result: &str = match hello {
+//     Some(ref x) => x,
+//     None => ""
+// };
+
+// This will work too, may be even simpler just for single strings
+// result = hello.map(|m| &m[..]).unwrap_or("");
+
+// requires a string of type: &str
+// println!("{}\n\n", result);
+
+// let file_path = "./hello.txt";
